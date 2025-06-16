50bb8c5d998b418be29be29662b0e2d684c2ae6a
Jon-Martin Storm <jonmartin.storm@gmail.com>
2025-06-16 10:38:06 +0200

    Trenger ikke graceful shutdown i denne appen.

--- cmd/reposnusern/main.go
+++ cmd/reposnusern/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"log/slog"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/jonmartinstorm/reposnusern/internal/config"
 	"github.com/jonmartinstorm/reposnusern/internal/fetcher"
@@ -15,18 +13,9 @@ import (
 )
 
 func main() {
-	// Context for graceful shutdown
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-	defer cancel()
+	ctx := context.Background()
 
 	logger.SetupLogger()
-
-	go func() {
-		<-ctx.Done()
-		slog.Info("SIGTERM mottatt – rydder opp...")
-		// Her kan vi legge til ekstra rydding om vi trenger det
-		// TODO sende context til dbcall og skriving av filer.
-	}()
 
 	cfg, err := config.NewConfig()
 	if err != nil {
